57872d91a3932816ea57188a216829fef2c8ab8a
olatubora-felix <olatubora1996@gmail.com>
2022-12-23 05:37:41 +0100

    useEffect replaced

--- src/components/UI/Message.jsx
+++ src/components/UI/Message.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Message = ({ text, className }) => {
   return (
-    <div className="mx-auto container my-4 text-2xl">
+    <div className="mx-auto container my-4 text-2xl p-4">
       <h1 className={className}>{text}</h1>
     </div>
   )
